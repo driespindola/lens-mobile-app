9628332e481ceb201244327cb88621d68709a51e
Dri Espindola <103765212+driespindola@users.noreply.github.com>
2023-03-08 09:53:44 -0300

    ajust profile screen

--- my-app/components/Profile Page/ProfileVideos.tsx
+++ my-app/components/Profile Page/ProfileVideos.tsx
@@ -1,17 +1,15 @@
 import { useQuery } from '@apollo/client';
 import React from 'react'
-import { View, Image, FlatList, Text, Dimensions } from 'react-native'
+import { View, FlatList, StyleSheet } from 'react-native'
 import { PaginatedPublicationResult, PublicationsDocument } from '../../types/lens';
-import getAvatar from '../../utils/getAvatar';
 import { Video, ResizeMode } from 'expo-av';
 import { sanitizeIpfsUrl } from '../../utils/sanitizeIpfsUrl';
-import VideoPlayer from 'expo-video-player';
 import { WINDOW_HEIGHT, WINDOW_WIDTH } from '../../utils/getHeight';
 
-const ProfileVideos = ({ profileId }: any) => {
+const videoWidth = WINDOW_WIDTH / 3 - 6;
+const videoHeight = videoWidth * 16 / 9;
 
-  const videoWidth = Dimensions.get('window').width / 3 - 6;
-  const videoHeight = videoWidth * 16 / 9;
+const ProfileVideos = ({ profileId }: any) => {
 
   const { data, loading, error } = useQuery
   <{publications: PaginatedPublicationResult}>
@@ -29,29 +27,17 @@ const ProfileVideos = ({ profileId }: any) => {
 
   const publications = data?.publications.items
 
+  console.log("Width", WINDOW_WIDTH)
+  console.log("Height", WINDOW_HEIGHT)
+
   return (
-    <View
-      style={{
-        width: WINDOW_WIDTH,
-        height: WINDOW_HEIGHT - 390
-      }}
-    >
+    <View style={styles.container}>
       <FlatList
-        style={{
-          marginTop: 51
-        }}
-        contentContainerStyle={{
-          flexGrow: 1,
-          justifyContent: 'center'
-        }}
+        style={styles.listStyle}
+        contentContainerStyle={styles.listContainerStyle}
         data={publications}
         renderItem={({ item, index }) => (
-          <View
-            style={{
-              flex: 1,
-              margin: 2,
-            }}
-          >
+          <View style={styles.videoContainer}>
             <Video
               shouldPlay={false}
               isLooping={true}
@@ -60,11 +46,7 @@ const ProfileVideos = ({ profileId }: any) => {
               source={{
                 uri: `${sanitizeIpfsUrl(item.metadata.media[0].original.url)}`
               }}
-              style={{
-                width: videoWidth,
-                height: videoHeight,
-                backgroundColor: 'black'
-              }}
+              style={styles.video}
             />
           </View>
         )}
@@ -73,5 +55,28 @@ const ProfileVideos = ({ profileId }: any) => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: WINDOW_WIDTH,
+    height: WINDOW_HEIGHT - 390
+  },
+  listStyle: {
+    marginTop: WINDOW_HEIGHT / 16 - 1
+  },
+  listContainerStyle: {
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+  videoContainer: {
+    flex: 1,
+    margin: 2,
+  },
+  video: {
+    width: videoWidth,
+    height: videoHeight,
+    backgroundColor: 'black'
+  },
+});
 
 export default ProfileVideos
